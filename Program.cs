a6938075b28877cb78b7d8643b750e395b7608c6
particlis <79189885+heh-gif-124@users.noreply.github.com>
2026-04-09 09:56:35 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+wizard wizardA = new wizard("Ralsei", 10);
+wizard wizardB = new wizard("John shaman",10);
+class wizard
+{
+    string Name;
+    int Energy;
+    int Damage;
+    public wizard(string name, int damage)
+    {
+        Name = name;
+        Energy = 100;
+        Damage = damage;
+    }
+
+    public void Attack(wizard enemyObj)
+    {
+        enemyObj.Energy -= Damage;
+        Console.WriteLine($"{Name} attacked {enemyObj.Name}");
+        Console.WriteLine($"{enemyObj.Name} has {enemyObj.Energy} left");
+    }
+}
