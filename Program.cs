7fb4f1cb0b0ec55c038cf9ee847602c251d6aedf
particlis <79189885+heh-gif-124@users.noreply.github.com>
2026-04-09 10:40:05 +0800

    HYUH

--- Program.cs
+++ Program.cs
@@ -1,15 +1,38 @@
 ﻿
 wizard wizardA = new wizard("Ralsei", 10);
 wizard wizardB = new wizard("John shaman",10);
-while (wizardA.Energy)
+while (wizardA.Energy > 0 && wizardB.Energy > 0)
 {
+    Console.WriteLine($"{wizardA.Name}'s Energy: {wizardA.Energy}");
+    Console.WriteLine($"{wizardB.Name}'s Energy: {wizardB.Energy}");
+    Console.WriteLine($"1. {wizardA.Name} attacks {wizardB.Name}");
+    Console.WriteLine($"2. {wizardB.Name} attacks {wizardA.Name}");
+    Console.WriteLine($"3. {wizardA.Name} heals");
+    Console.WriteLine($"4. {wizardB.Name} heals");
+    string input = Console.ReadLine();
+    switch (input)
+    {
+        case "1":
+            wizardA.Attack(wizardB); break;
+        case "2":
+            wizardB.Attack(wizardA); break;
+        case "3":
+            wizardA.heal();
+            break;
+        case "4":
+            wizardB.heal();
+            break;
+        default:
+            Console.WriteLine("Thats not a command!!");
+            break;
+    }
 
 }
 class wizard
 {
-    string Name;
+    public string Name;
     public int Energy;
-    int Damage;
+    public int Damage;
     public wizard(string name, int damage)
     {
         Name = name;
